57c742ad5f8c9d12fa06a80c1dcc8770988efdcc
AshOne91 <ldgo91@gmail.com>
2022-12-12 21:37:29 +0900

    feat : add config reader, add default ini

--- source/Elephant_Engine/Config.h
+++ source/Elephant_Engine/Config.h
@@ -6,8 +6,11 @@
 
 class Config : public Singleton<Config>
 {
+private:
+	std::string local_path_
+
 public:
-	std::string local_path_;
+	std::string shader_path_;
 
 public:
 	Config() : local_path_{ std::filesystem::current_path().generic_string() } {}
